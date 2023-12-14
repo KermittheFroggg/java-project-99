e75d08fde4078764423683cb6cfa87e0fa0c0adb
KermittheFroggg <santamantana@gmail.com>
2023-12-14 17:54:24 +0100

    step 8

--- src/main/java/hexlet/code/controller/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/AuthenticationController.java
@@ -3,10 +3,13 @@ package hexlet.code.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,13 +27,16 @@ public final class AuthenticationController {
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/api/login")
-    public String create(@RequestBody AuthRequest authRequest) {
-        var authentication = new UsernamePasswordAuthenticationToken(
-                authRequest.getUsername(), authRequest.getPassword());
+    public ResponseEntity<String> create(@RequestBody AuthRequest authRequest) {
+        try {
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    authRequest.getUsername(), authRequest.getPassword());
 
-        authenticationManager.authenticate(authentication);
+            authenticationManager.authenticate(authentication);
 
-        var token = jwtUtils.generateToken(authRequest.getUsername());
-        return token;
-    }
-}
+            var token = jwtUtils.generateToken(authRequest.getUsername());
+            return ResponseEntity.ok(token);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }}
