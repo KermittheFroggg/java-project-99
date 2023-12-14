4a570ca9e2e8332448ef9c17bcf8b68de69c3795
KermittheFroggg <santamantana@gmail.com>
2023-12-14 18:39:06 +0100

    step 8

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -2,9 +2,9 @@ package hexlet.code.config;
 
 import hexlet.code.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -27,14 +27,17 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtDecoder jwtDecoder;
 
-    private final UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    private final JwtDecoder jwtDecoder;
+    @Autowired
+    private UserService userService;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector)
@@ -43,29 +46,16 @@ public class SecurityConfig {
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(mvcMatcherBuilder.pattern("/swagger-ui.html"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern("/swagger-ui/**"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern("/v3/api-docs/**"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern("/"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern("/index.html"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern("/welcome"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, "/assets/**"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST, "/api/login"))
-                        .permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST, "/api/users"))
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
-                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(mvcMatcherBuilder.pattern("/index.html")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/assets/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/welcome")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/login")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/v3/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/swagger-ui/***")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern(POST, "/api/users")).permitAll()
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer((rs) -> rs.jwt((jwt) -> jwt.decoder(jwtDecoder)))
                 .httpBasic(Customizer.withDefaults())
