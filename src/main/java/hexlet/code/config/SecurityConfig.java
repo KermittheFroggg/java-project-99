cf84bfeddf0493db3d3d21a30ab2eaf5be0ae835
KermittheFroggg <santamantana@gmail.com>
2023-12-16 13:52:53 +0100

    step 8

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package hexlet.code.config;
 
 import hexlet.code.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
