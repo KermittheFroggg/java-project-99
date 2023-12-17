edc3b3f7d58d1f91bc2123ee3a72c59dabff5173
KermittheFroggg <santamantana@gmail.com>
2023-12-17 12:36:39 +0100

    step 8

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,8 +1,12 @@
 package hexlet.code.repository;
 
 import hexlet.code.model.Task;
+import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByName(String name);
 }
